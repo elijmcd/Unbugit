50313c6613ebb9a0c858b92b963a0709059d6676
elijmcd <elijmcd@gmail.com>
2021-05-24 16:30:09 -0500

    string length validation for name inputs

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,12 +52,12 @@ namespace Unbugit.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [EmailAddress]
+            [StringLength(33, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
             [Display(Name = "First Name")]
             public string FirstName { get; set; }
 
             [Required]
-            [EmailAddress]
+            [StringLength(33, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
 
