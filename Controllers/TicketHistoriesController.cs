740ffd0e7917869fae14cf752ec9a184bb79519b
elijmcd <elijmcd@gmail.com>
2021-05-25 16:28:08 -0500

    initial controller scaffolding complete

---
+++ Controllers/TicketHistoriesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Unbugit.Data;
+using Unbugit.Models;
+
+namespace Unbugit.Controllers
+{
+    public class TicketHistoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TicketHistoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TicketHistories
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.TicketHistory.Include(t => t.Ticket);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: TicketHistories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistory
+                .Include(t => t.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Create
+        public IActionResult Create()
+        {
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Description");
+            return View();
+        }
+
+        // POST: TicketHistories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TicketId,Property,OldValue,NewValue,Created,UserId,Description")] TicketHistory ticketHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticketHistory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Description", ticketHistory.TicketId);
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistory.FindAsync(id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Description", ticketHistory.TicketId);
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TicketId,Property,OldValue,NewValue,Created,UserId,Description")] TicketHistory ticketHistory)
+        {
+            if (id != ticketHistory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticketHistory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketHistoryExists(ticketHistory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Ticket, "Id", "Description", ticketHistory.TicketId);
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistory
+                .Include(t => t.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticketHistory = await _context.TicketHistory.FindAsync(id);
+            _context.TicketHistory.Remove(ticketHistory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketHistoryExists(int id)
+        {
+            return _context.TicketHistory.Any(e => e.Id == id);
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,5 +13,17 @@ namespace Unbugit.Data
             : base(options)
         {
         }
+        public DbSet<Unbugit.Models.Company> Company { get; set; }
+        public DbSet<Unbugit.Models.Invite> Invite { get; set; }
+        public DbSet<Unbugit.Models.Notification> Notification { get; set; }
+        public DbSet<Unbugit.Models.Project> Project { get; set; }
+        public DbSet<Unbugit.Models.ProjectPriority> ProjectPriority { get; set; }
+        public DbSet<Unbugit.Models.Ticket> Ticket { get; set; }
+        public DbSet<Unbugit.Models.TicketAttachment> TicketAttachment { get; set; }
+        public DbSet<Unbugit.Models.TicketComment> TicketComment { get; set; }
+        public DbSet<Unbugit.Models.TicketHistory> TicketHistory { get; set; }
+        public DbSet<Unbugit.Models.TicketPriority> TicketPriority { get; set; }
+        public DbSet<Unbugit.Models.TicketStatus> TicketStatus { get; set; }
+        public DbSet<Unbugit.Models.TicketType> TicketType { get; set; }
     }
 }
