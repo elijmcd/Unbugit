b10084185f48d800aabefc48be44c805100e3a88
elijmcd <elijmcd@gmail.com>
2021-05-26 11:23:13 -0500

    added BTRoles interface & service

---
+++ Services/BTRoleService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unbugit.Models;
+using Unbugit.Services.Interfaces;
+
+namespace Unbugit.Services
+{
+    public class BTRolesService : IBTRoleService
+    {
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> ListUserRolesAsync(BTUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BTUser>> UsersNotInRoleAsync(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interfaces/IBTRoleService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unbugit.Models;
+
+namespace Unbugit.Services.Interfaces
+{
+    public interface IBTRoleService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+        public Task<IEnumerable<string>> ListUserRolesAsync(BTUser user);
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        public Task<List<BTUser>> UsersNotInRoleAsync(string roleName);
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+
+    }
+}
